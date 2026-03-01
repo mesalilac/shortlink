546c6a7f6622144faa5d5a5d658521ea0360981c
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-03-02 02:03:59 +0300

    remove params

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,14 +1,7 @@
-import { useParams } from '@solidjs/router';
 import type { Component } from 'solid-js';
 
 import styles from './HomePage.module.css';
 
-type Params = {
-    id: string;
-};
-
 export const HomePage: Component = () => {
-    const params = useParams<Params>();
-
-    return <div>HomePage: {params.id}</div>;
+    return <div>HomePage</div>;
 };
