19a0e1b471256059630b04f6a38ad6f9d5d0b097
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-03-02 01:42:13 +0300

    setup biome and stylelint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,12 @@ import type { Component } from 'solid-js';
 import Comp from './Comp';
 
 const App: Component = () => {
-  return (
-    <>
-      <h1>Hello world!!!!</h1>
-      <Comp />
-    </>
-  );
+    return (
+        <>
+            <h1>Hello world!!!!</h1>
+            <Comp />
+        </>
+    );
 };
 
 export default App;
