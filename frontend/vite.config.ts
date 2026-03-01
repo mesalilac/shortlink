0064fc2b1c45f48f9598ec2d19ee531243ca7ce9
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-03-02 01:45:02 +0300

    setup ts paths

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,21 +1,22 @@
-import devtools from "solid-devtools/vite";
-import { defineConfig } from "vite";
-import solidPlugin from "vite-plugin-solid";
+import devtools from 'solid-devtools/vite';
+import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-	plugins: [devtools(), solidPlugin()],
-	server: {
-		port: 3000,
-		proxy: {
-			"/api": {
-				target: "http://localhost:8080",
-				changeOrigin: true,
-				secure: false,
-			},
-		},
-	},
-	build: {
-		outDir: "./dist",
-		target: "esnext",
-	},
+    plugins: [devtools(), solidPlugin(), tsconfigPaths()],
+    server: {
+        port: 3000,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+                secure: false,
+            },
+        },
+    },
+    build: {
+        outDir: './dist',
+        target: 'esnext',
+    },
 });
